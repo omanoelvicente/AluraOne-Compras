7c1d9cce146c9885c9278d47786747da55f35c83
Manoel Vicente <manoelj.vicente@gmail.com>
2024-11-24 00:02:48 +0000

    :sparkles: feat: Add classes Item.java and CarrinhoDeCompra.java

--- src/CarrinhoDeCompras.java
+++ src/CarrinhoDeCompras.java
@@ -1,2 +1,41 @@
+import java.util.*;
+
+import static java.lang.System.in;
+
 public class CarrinhoDeCompras {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(in);
+        List<Item> lista = new ArrayList<>();
+
+
+        System.out.println("Digite o limite do cartão");
+        double valorLimite = scanner.nextDouble();
+        CartaoCredito meuCartao = new CartaoCredito(valorLimite);
+        System.out.println("Seu limite R$:" + meuCartao.getLimite());
+        int i = 1;
+        while (i != 0) {
+            System.out.println("Digite a descrição da compra");
+            String descricaoItem = scanner.next();
+
+            System.out.println("Digite o valor da compra");
+            double valorItem = scanner.nextDouble();
+            Item meuItem = new Item(descricaoItem, valorItem);
+
+            if (meuCartao.verificaSaldo(meuItem.getValorItem())) {
+                System.out.println("Compra realizada dom sucesso!");
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+            System.out.println("Escolha uma opção: [0] - Sair [1] - Continuar comprando");
+            i = (scanner.nextInt());
+            lista.add(meuItem);
+            System.out.flush();
+        }
+        Collections.sort(lista);
+        lista.forEach(System.out::println);
+        System.out.println("Saldo: " + meuCartao.getSaldo());
+
+    }
+
 }
--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,36 @@
-public class Item {
+import java.util.Objects;
+
+public class Item implements Comparable<Item> {
+    private String descricaoItem;
+    private double valorItem;
+
+
+
+    public Item(String descricaoItem, double valorItem) {
+        this.descricaoItem = descricaoItem;
+        this.valorItem = valorItem;
+    }
+    public String getDescricaoItem() {
+        return descricaoItem;
+    }
+
+
+    public double getValorItem() {
+        return valorItem;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Descrição: %s
+                Valor R$:%.2f
+                --------------------
+                """, this.getDescricaoItem(), this.getValorItem());
+    }
+
+    @Override
+    public int compareTo(Item outroItem) {
+        return (int) (this.getValorItem() - outroItem.getValorItem());
+    }
 }
